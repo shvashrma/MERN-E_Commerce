40d387480613aaa53610e360ebaf6aa68b5b2245
shvashrma <shvashrma643@gmail.com>
2023-12-05 16:19:58 +0530

    Redux slices are created for gettingallProducts and GetSingleProduct

--- client/src/Redux/Slices/productSlice.js
+++ client/src/Redux/Slices/productSlice.js
@@ -7,6 +7,7 @@ const initialState = {
   newProduct: null,
   error: null,
   products: null,
+  product: null,
 };
 
 const uploadNewProduct = createAsyncThunk(
@@ -18,10 +19,36 @@ const uploadNewProduct = createAsyncThunk(
         Authorization: `Bearer ${authToken}`,
       };
       const { data } = await axios.post("api/v1/product/new", formData, {
-          headers,
-          "Content-Type": "multipart/form-data",
-        })
-        console.log(data)
+        headers,
+        "Content-Type": "multipart/form-data",
+      });
+      console.log(data);
+      return data;
+    } catch (error) {
+      return error;
+    }
+  }
+);
+
+const gettingAllProduct = createAsyncThunk(
+  "product/gettingAllProduct",
+  async () => {
+    try {
+      const { data } = await axios.get("/api/v1/products");
+      console.log(data);
+      return data;
+    } catch (error) {
+      return error;
+    }
+  }
+);
+
+const getSingleProduct = createAsyncThunk(
+  "product/getSingleProduct",
+  async (productId) => {
+    try {
+      const { data } = await axios.get(`/api/v1/product/${productId}`);
+      console.log(data);
       return data;
     } catch (error) {
       return error;
@@ -49,10 +76,49 @@ const productSlice = createSlice({
           (state.newProduct = null),
           (state.status = "failed"),
           (state.error = action.error);
+      })
+      .addCase(gettingAllProduct.pending, (state) => {
+        (state.loading = true), (state.status = "pending");
+        state.newProduct = null;
+      })
+      .addCase(gettingAllProduct.fulfilled, (state, action) => {
+        (state.loading = false),
+          (state.status = "successfull"),
+          (state.products = action.payload),
+          (state.newProduct = null),
+          (state.error = null),
+          (state.product = null);
+      })
+      .addCase(gettingAllProduct.rejected, (state, action) => {
+        (state.loading = false),
+          (state.status = "rejected"),
+          (state.product = null),
+          (state.error = action.error),
+          (state.products = null),
+          (state.newProduct = null);
+      })
+      .addCase(getSingleProduct.pending, (state) => {
+        (state.loading = true), (state.status = "pending");
+      })
+      .addCase(getSingleProduct.fulfilled, (state, action) => {
+        (state.loading = false),
+          (state.status = "successful"),
+          (state.error = null),
+          (state.products = null),
+          (state.product = action.payload),
+          (state.newProduct = null);
+      })
+      .addCase(getSingleProduct.rejected, (state, action) => {
+        (state.loading = false),
+          (state.status = "rejected"),
+          (state.error = action.error),
+          (state.products = null),
+          (state.product = null),
+          (state.newProduct = null);
       });
   },
 });
 
-export { uploadNewProduct };
+export { uploadNewProduct, gettingAllProduct, getSingleProduct };
 
 export default productSlice.reducer;
